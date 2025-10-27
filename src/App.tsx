b4c3137d78b7a25c5436eef5e6fba1171112cd90
CandelaStaccia <candela.staccia@gmail.com>
2025-10-27 12:49:17 -0300

    authprovider app

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import LogIn from './pages/LogIn';
 import Register from './pages/Register';
 import Feed from './pages/Feed';
 import UserProfile from './pages/UserProfile';
+import AuthProvider from './context/AuthProvider';
 
 function App() {
-  
+  //no se a quienes deberia envolver AuthProvider
   return (
     <>
-      <Routes>
-        <Route path='/' element={<LogIn/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/feed' element={<Feed/>}/>
-        <Route path='/profile' element={<UserProfile/>}/>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AuthProvider> 
+        <Routes>
+          <Route path='/' element={<LogIn/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/feed' element={<Feed/>}/>
+          <Route path='/profile' element={<UserProfile/>}/>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AuthProvider>
     </>
   )
 }
