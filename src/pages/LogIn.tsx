c2107e92210c6b775ac497c4c3f06d86b6681ae7
CandelaStaccia <candela.staccia@gmail.com>
2025-10-26 22:23:43 -0300

    modificaciones estilos

--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import FormLogin from "../components/FormLogin";
 import styles from "./LogIn.module.css"
-import unahurimg from "../assets/unahurimg.jpg";
+import antisocialnet from "../assets/antisocialnet.jpg";
 import {Link} from "react-router-dom"
 
 function LogIn() {
@@ -14,24 +14,19 @@ function LogIn() {
 
   return (
     <main>
-      
-      <div className={styles.titulo}>
-        <h1>UnaHur Anti-Social Net</h1>
-      </div>
-
-      <Container className="d-flex justify-content-center align-items-center">
+      <Container fluid className="d-flex justify-content-center align-items-center" style={{minHeight: '100vh'}}>
         <Row className={styles.loginCardContainer}>
           <Col md={5} className="d-none d-md-block p-0">
-            <img src={unahurimg} alt="Login" className="img-fluid h-100 w-100" style={{objectFit: "cover"}}/>
+            <img src={antisocialnet} alt="Login" className="img-fluid h-100 w-100" style={{objectFit: "cover"}}/>
           </Col>
 
           <Col md={7} className="d-flex justify-content-center align-items-start">
             <div className={styles.divForm}>
               <div className={styles.mobileLogo}>
-                <img src={unahurimg} alt="Logo"/>
+                <img src={antisocialnet} alt="Logo"/>
               </div>
               <h2>Iniciar sesión</h2>
-              <p className="mb-o d-inline">No tenés una cuenta?</p> 
+              <p className="mb-o d-inline">¿No tenés una cuenta?</p> 
               <Link to= "/register" className={`${styles.link} ms-1`}>Registrarse</Link>
               <FormLogin/>
             </div>
