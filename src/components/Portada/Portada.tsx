e29f76d524d1969002f77916086114408d72e733
CandelaStaccia <candela.staccia@gmail.com>
2025-10-29 19:27:52 -0300

    arreglo animacion

--- src/components/Portada/Portada.tsx
+++ src/components/Portada/Portada.tsx
@@ -10,11 +10,12 @@ export default function Portada() {
   return (
     <>
         <Col md={6}>
-            <img src={antisocialpng} alt="UnaHur Anti-Social Net" className={`${styles.portadaImg} ${styles.aparicion}`}/>
+        <figure className={styles.aparicion}>
+            <img src={antisocialpng} alt="UnaHur Anti-Social Net" className={styles.portadaImg}/>
+        </figure>
         </Col>
         <Col md={6}>
             <p className={styles.descripcion}>Conectá con personas, compartí tus ideas y descubrí nuevos contenidos</p>
-            
             <div className={styles.botones}>
                 <Button className={styles.loginBtn} onClick={() => navigate("/login")}>Iniciar sesión</Button>
                 <Button className={styles.registerBtn} onClick={() => navigate("/register")}>Registrarme</Button>
